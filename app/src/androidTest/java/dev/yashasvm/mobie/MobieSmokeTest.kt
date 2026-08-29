903b9a313bf3ceed7a9565524f582dbac3c18d4c
YashasVM <yashasvmtvzzz@gmail.com>
2026-08-29 13:30:25 +0530

    test: wait for splash completion

--- app/src/androidTest/java/dev/yashasvm/mobie/MobieSmokeTest.kt
+++ app/src/androidTest/java/dev/yashasvm/mobie/MobieSmokeTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.fetchSemanticsNodes
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
