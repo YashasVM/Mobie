4f07f10311fbe7d69cd103763283b1b614d4b6d7
YashasVM <admin5@duck.com>
2026-09-03 10:32:21 +0530

    Bump versionCode to 3 for v0.2.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "dev.yashasvm.mobie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
